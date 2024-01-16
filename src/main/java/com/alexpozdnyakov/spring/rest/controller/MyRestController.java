d1e063e1d1dc484cd4c0f9b7b8d2e941343c8df6
lehagorinich <Ratatui163@ya.ru>
2024-01-17 00:43:37 +0400

    spring boot

--- src/main/java/com/alexpozdnyakov/spring/rest/controller/MyRestController.java
+++ src/main/java/com/alexpozdnyakov/spring/rest/controller/MyRestController.java
@@ -22,45 +22,34 @@ public class MyRestController {
 
     @GetMapping("/employees")
     public List<Employee> showAllEmployees() {
-        //List<Employee> allEmployees=employeeService.getAllEmployees();
-        //return allEmployees;
         return employeeService.getAllEmployees();
     }
 
     @GetMapping("/employees/{id}")
-
     public Employee getEmployee(@PathVariable int id) {
         Employee employee = employeeService.getEmployee(id);
-
-
         if (employee == null) {
             throw new NoSuchEmployeeException("There is no enployee whith id " + id + " int Database");
         }
         return employee;
-
-
     }
+
     @PostMapping("/employees")
-    public Employee addNewEmployee(@RequestBody Employee employee){
+    public Employee addNewEmployee(@RequestBody Employee employee) {
         employeeService.saveEmployee(employee);
         return employee;
     }
 
     @PutMapping("/employees")
-    public Employee updateEmployee(@RequestBody Employee employee){
+    public Employee updateEmployee(@RequestBody Employee employee) {
         employeeService.saveEmployee(employee);
         return employee;
     }
+
     @DeleteMapping("/employees/{id}")
-    public String deleteEmployee(@PathVariable int id){
-        Employee employee=employeeService.getEmployee(id);
-        if(employee==null){
-            throw  new NoSuchEmployeeException("There is no enployee whith id " + id + " int Database");
-        }
+    public void deleteEmployee(@PathVariable int id) {
         employeeService.deleteEmployee(id);
-        return "Employee with ID = "+id+" was deleted";
     }
-
-
-
 }
+
+
